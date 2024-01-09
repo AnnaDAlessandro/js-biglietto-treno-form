d2816603dc30f9b04b9b73acba267f7c1d90fe42
AnnaDAlessandro <annadalex@icloud.com>
2024-01-09 18:14:08 +0100

    Correzione esercizio

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const nomeCognomeHtml = document.getElementById(`nameSurname`)
+const kilometriHtml = document.getElementById(`Km`)
+const FasciaEtaHtml = document.getElementById(`eta`)
+const bottoneRisultato = document.getElementById(`bottoneGenera`)
+
+bottoneRisultato.addEventListener(`click`,function () {
+    document.querySelector(`h2.d-none`).classList.remove(`d-none`)
+    document.querySelector(`div.d-none`).classList.remove(`d-none`)
+
+})
+    
